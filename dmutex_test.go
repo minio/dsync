540beac793cebf7d086c77818bb1321c21ef4ec9
frankw <fwessels@xs4all.nl>
2016-08-01 15:07:36 -0700

    Test three simultaneous locks

--- dmutex_test.go
+++ dmutex_test.go
@@ -69,4 +69,69 @@ func TestTwoSimultaneousLocksForSameResource(t *testing.T) {
 	time.Sleep(2500 * time.Millisecond)
 
 	dm2nd.Unlock()
+
+// Test three locks for same resource, one succeeds, one fails (after timeout)
+func TestThreeSimultaneousLocksForSameResource(t *testing.T) {
+
+	dm1st := DMutex{name: "aap"}
+	dm2nd := DMutex{name: "aap"}
+	dm3rd := DMutex{name: "aap"}
+
+	dm1st.Lock()
+
+	// Release lock after 10 seconds
+	go func() {
+		time.Sleep(10 * time.Second)
+		fmt.Println("Unlocking dm1")
+
+		dm1st.Unlock()
+	}()
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go func() {
+		defer wg.Done()
+
+		dm2nd.Lock()
+
+		// Release lock after 10 seconds
+		go func() {
+			time.Sleep(2500 * time.Millisecond)
+			fmt.Println("Unlocking dm2")
+
+			dm2nd.Unlock()
+		}()
+
+		dm3rd.Lock()
+
+		fmt.Printf("3rd lock obtained after 1st & 2nd locks are released\n")
+		time.Sleep(2500 * time.Millisecond)
+
+		dm3rd.Unlock()
+	}()
+
+	go func() {
+		defer wg.Done()
+
+		dm3rd.Lock()
+
+		// Release lock after 10 seconds
+		go func() {
+			time.Sleep(2500 * time.Millisecond)
+			fmt.Println("Unlocking dm3")
+
+			dm3rd.Unlock()
+		}()
+
+		dm2nd.Lock()
+
+		fmt.Printf("2nd lock obtained after 1st & 3rd locks are released\n")
+		time.Sleep(2500 * time.Millisecond)
+
+		dm2nd.Unlock()
+	}()
+
+	wg.Wait()
+}
 }
