7e908dcd129f1709d3b2aae62b61c4248b899375
frankw <fwessels@xs4all.nl>
2016-09-08 17:01:58 +0200

    Update chaos server with latest server side code

--- chaos/lock-rpc-server.go
+++ chaos/lock-rpc-server.go
@@ -17,79 +17,100 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"github.com/minio/dsync"
 	"sync"
+	"time"
 )
 
-type Locker struct {
-	mu sync.Mutex
-	// e.g, when a Lock(name) is held, map[string][]bool{"name" : []bool{true}}
-	// when one or more RLock() is held, map[string][]bool{"name" : []bool{false, false}}
-	nsMap map[string][]bool
+// used when cached timestamp do not match with what client remembers.
+var errInvalidTimestamp = errors.New("Timestamps don't match, server may have restarted.")
+
+const WriteLock = -1
+
+type lockServer struct {
+	mutex sync.Mutex
+						// Map of locks, with negative value indicating (exclusive) write lock
+						// and positive values indicating number of read locks
+	lockMap   map[string]int64
+	timestamp time.Time // Timestamp set at the time of initialization. Resets naturally on minio server restart.
 }
 
-func (locker *Locker) Lock(args *dsync.LockArgs, reply *bool) error {
-	locker.mu.Lock()
-	defer locker.mu.Unlock()
-	_, ok := locker.nsMap[args.Name]
-	if !ok {
+func (l *lockServer) verifyArgs(args *dsync.LockArgs) error {
+	if !l.timestamp.Equal(args.Timestamp) {
+		return errInvalidTimestamp
+	}
+	return nil
+}
+
+func (l *lockServer) Lock(args *dsync.LockArgs, reply *bool) error {
+	l.mutex.Lock()
+	defer l.mutex.Unlock()
+	if err := l.verifyArgs(args); err != nil {
+		return err
+	}
+	if _, *reply = l.lockMap[args.Name]; !*reply {
+		l.lockMap[args.Name] = WriteLock // No locks held on the given name, so claim write lock
+	}
+	*reply = !*reply // Negate *reply to return true when lock is granted or false otherwise
+	return nil
+}
+
+func (l *lockServer) Unlock(args *dsync.LockArgs, reply *bool) error {
+	l.mutex.Lock()
+	defer l.mutex.Unlock()
+	if err := l.verifyArgs(args); err != nil {
+		return err
+	}
+	var locksHeld int64
+	if locksHeld, *reply = l.lockMap[args.Name]; !*reply { // No lock is held on the given name
+		return fmt.Errorf("Unlock attempted on an unlocked entity: %s", args.Name)
+	}
+	if *reply = locksHeld == WriteLock; !*reply { // Unless it is a write lock
+		return fmt.Errorf("Unlock attempted on a read locked entity: %s (%d read locks active)", args.Name, locksHeld)
+	}
+	delete(l.lockMap, args.Name) // Remove the write lock
+	return nil
+}
+
+const ReadLock = 1
+
+func (l *lockServer) RLock(args *dsync.LockArgs, reply *bool) error {
+	l.mutex.Lock()
+	defer l.mutex.Unlock()
+	if err := l.verifyArgs(args); err != nil {
+		return err
+	}
+	var locksHeld int64
+	if locksHeld, *reply = l.lockMap[args.Name]; !*reply {
+		l.lockMap[args.Name] = ReadLock // No locks held on the given name, so claim (first) read lock
 		*reply = true
-		locker.nsMap[args.Name] = []bool{true}
-		return nil
-	}
-	*reply = false
-	return nil
-}
-
-func (locker *Locker) Unlock(args *dsync.LockArgs, reply *bool) error {
-	locker.mu.Lock()
-	defer locker.mu.Unlock()
-	_, ok := locker.nsMap[args.Name]
-	if !ok {
-		return fmt.Errorf("Unlock attempted on an un-locked entity: %s", args.Name)
-	}
-	*reply = true
-	delete(locker.nsMap, args.Name)
-	return nil
-}
-
-func (locker *Locker) RLock(args *dsync.LockArgs, reply *bool) error {
-	locker.mu.Lock()
-	defer locker.mu.Unlock()
-	locksHeld, ok := locker.nsMap[args.Name]
-	if !ok {
-		// First read-lock to be held on *name.
-		locker.nsMap[args.Name] = []bool{false}
 	} else {
-		// Add an entry for this read lock.
-		if len(locker.nsMap[args.Name]) == 1 && locker.nsMap[args.Name][0] == true {
-			*reply = false
-			return nil
+		if *reply = locksHeld != WriteLock; *reply { // Unless there is a write lock
+			l.lockMap[args.Name] = locksHeld + ReadLock // Grant another read lock
 		}
-		locker.nsMap[args.Name] = append(locksHeld, false)
 	}
-	*reply = true
-
 	return nil
 }
 
-func (locker *Locker) RUnlock(args *dsync.LockArgs, reply *bool) error {
-	locker.mu.Lock()
-	defer locker.mu.Unlock()
-	locksHeld, ok := locker.nsMap[args.Name]
-	if !ok {
-		return fmt.Errorf("RUnlock attempted on an un-locked entity: %s", args.Name)
+func (l *lockServer) RUnlock(args *dsync.LockArgs, reply *bool) error {
+	l.mutex.Lock()
+	defer l.mutex.Unlock()
+	if err := l.verifyArgs(args); err != nil {
+		return err
 	}
-	if len(locksHeld) > 1 {
-		// Remove one of the read locks held.
-		locksHeld = locksHeld[1:]
-		locker.nsMap[args.Name] = locksHeld
+	var locksHeld int64
+	if locksHeld, *reply = l.lockMap[args.Name]; !*reply { // No lock is held on the given name
+		return fmt.Errorf("RUnlock attempted on an unlocked entity: %s", args.Name)
+	}
+	if *reply = locksHeld != WriteLock; !*reply { // A write-lock is held, cannot release a read lock
+		return fmt.Errorf("RUnlock attempted on a write locked entity: %s", args.Name)
+	}
+	if locksHeld > ReadLock {
+		l.lockMap[args.Name] = locksHeld - ReadLock // Remove one of the read locks held
 	} else {
-		// Delete the map entry since this is the last read lock held
-		// on *name.
-		delete(locker.nsMap, args.Name)
+		delete(l.lockMap, args.Name) // Remove the (last) read lock
 	}
-	*reply = true
 	return nil
 }
